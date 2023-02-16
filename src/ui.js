fe96b33a04cbd5a1502eddd996ec26209ff9b8d7
cryptonomicon46 <sandman2145@gmail.com>
2023-02-16 09:35:02 -0800

    Micro posts project created

---
+++ src/ui.js
@@ -0,0 +1,65 @@
+class UI {
+  constructor() {
+    this.post = document.querySelector("#posts");
+    this.titleInput = document.querySelector("#title");
+    this.bodyInput = document.querySelector("#body");
+    this.idInput = document.querySelector("#id");
+    this.postSubmit = document.querySelector(".post-submit");
+    this.forState = "add";
+  }
+
+  showPosts(posts) {
+    let output = "";
+    posts.forEach((post) => {
+      output += `
+        <div class = "card mb-3">
+            <div class = "card-body">
+                <h4 class = "card-title">${post.title}</h4>
+                <p class = "card-text">${post.body}</p>
+                <a href="#" class="edit card-link" data-id="${post.id}">
+
+                <i class = "fa fa-pencil"></i>
+                </a>    
+                <a href ="#" class = "delete card-link" data-id="${post.id}">
+                <i class = "fa fa-remove"></i>
+                </a>    
+            </div>
+        </div>
+        `;
+    });
+    this.post.innerHTML = output;
+  }
+
+  showAlert(msg, className) {
+    this.clearAlert();
+
+    //Create div
+    const div = document.createElement("div");
+    //Add classes
+    div.className = className;
+    //Add text
+    div.appendChild(document.createTextNode(msg));
+    //Get Parent
+    const container = document.querySelector(".postsContainer");
+    //Get Posts
+    const posts = document.querySelector("#posts");
+    //Insert alert div
+    container.insertBefore(div, posts);
+    //Timeout
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+  clearAlert() {
+    const curentAlert = document.querySelector(".alert");
+    if (curentAlert) {
+      curentAlert.remove();
+    }
+  }
+  clearFields() {
+    this.titleInput.value = "";
+    this.bodyInput.value = "";
+  }
+}
+
+export const ui = new UI();
