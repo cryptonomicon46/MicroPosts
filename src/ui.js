2f315bd9a97126a3b7dacde056cd39b0b9fb6b7a
cryptonomicon46 <sandman2145@gmail.com>
2023-02-16 09:57:36 -0800

    Recreated all project files

--- src/ui.js
+++ src/ui.js
@@ -1,64 +1,81 @@
 class UI {
   constructor() {
-    this.post = document.querySelector("#posts");
-    this.titleInput = document.querySelector("#title");
-    this.bodyInput = document.querySelector("#body");
-    this.idInput = document.querySelector("#id");
-    this.postSubmit = document.querySelector(".post-submit");
-    this.forState = "add";
+    this.post = document.querySelector('#posts');
+    this.titleInput = document.querySelector('#title');
+    this.bodyInput = document.querySelector('#body');
+    this.idInput = document.querySelector('#id');
+    this.postSubmit = document.querySelector('.post-submit');
+    this.forState = 'add';
   }
 
+  // Show all posts
   showPosts(posts) {
-    let output = "";
+    let output = '';
+
     posts.forEach((post) => {
       output += `
-        <div class = "card mb-3">
-            <div class = "card-body">
-                <h4 class = "card-title">${post.title}</h4>
-                <p class = "card-text">${post.body}</p>
-                <a href="#" class="edit card-link" data-id="${post.id}">
+        <div class="card mb-3">
+          <div class="card-body">
+            <h4 class="card-title">${post.title}</h4>
+            <p class="card-text">${post.body}</p>
+            <a href="#" class="edit card-link" data-id="${post.id}">
+              <i class="fa fa-pencil"></i>
+            </a>
 
-                <i class = "fa fa-pencil"></i>
-                </a>    
-                <a href ="#" class = "delete card-link" data-id="${post.id}">
-                <i class = "fa fa-remove"></i>
-                </a>    
-            </div>
+            <a href="#" class="delete card-link" data-id="${post.id}">
+            <i class="fa fa-remove"></i>
+          </a>
+          </div>
         </div>
-        `;
+      `;
     });
+
     this.post.innerHTML = output;
   }
 
-  showAlert(msg, className) {
+  // Show alert message
+  showAlert(message, className) {
     this.clearAlert();
 
-    //Create div
-    const div = document.createElement("div");
-    //Add classes
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
     div.className = className;
-    //Add text
-    div.appendChild(document.createTextNode(msg));
-    //Get Parent
-    const container = document.querySelector(".postsContainer");
-    //Get Posts
-    const posts = document.querySelector("#posts");
-    //Insert alert div
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.postsContainer');
+    // Get posts
+    const posts = document.querySelector('#posts');
+    // Insert alert div
     container.insertBefore(div, posts);
-    //Timeout
+
+    // Timeout
     setTimeout(() => {
       this.clearAlert();
     }, 3000);
   }
+
+  // Clear alert message
   clearAlert() {
-    const curentAlert = document.querySelector(".alert");
-    if (curentAlert) {
-      curentAlert.remove();
+    const currentAlert = document.querySelector('.alert');
+
+    if(currentAlert) {
+      currentAlert.remove();
     }
   }
+
+  // Clear all fields
   clearFields() {
-    this.titleInput.value = "";
-    this.bodyInput.value = "";
+    this.titleInput.value = '';
+    this.bodyInput.value = '';
+  }
+
+  // Fill form to edit
+  fillForm(data) {
+    this.titleInput.value = data.title;
+    this.bodyInput.value = data.body;
+    this.idInput.value = data.id;
   }
 }
 
